1a14fba66a132b24664f67b0875ae2ebfbaf41db
David VanDusen <github@davidvandusen.com>
2016-01-14 15:18:42 -0800

    Improve home page stats and date format.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,22 @@ class PagesController < ApplicationController
 
   def home
     @page = {
-      questions_count: Question.count,
-      cohorts_count: Cohort.count,
-      activities_count: Activity.count,
-      quizzes_count: Quiz.count
+      questions: model_stats(Question),
+      cohorts: model_stats(Cohort),
+      activities: model_stats(Activity),
+      quizzes: model_stats(Quiz),
+      users: model_stats(User)
     }
     @quiz = Quiz.new
+  end
+
+  protected
+
+  def model_stats(model)
+    {
+      count: model.count,
+      last_updated: model.unscoped.select(:updated_at).order(updated_at: :desc).limit(1).first.try(:updated_at)
+    }
   end
 
 end
