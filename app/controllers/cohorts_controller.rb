2e803d053a21cc6cb3d91e565cd329bb5bb7eb7c
David VanDusen <github@davidvandusen.com>
2016-01-15 11:32:18 -0800

    Improve submission stats on cohort page.

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -7,6 +7,17 @@ class CohortsController < ApplicationController
     @cohort = Cohort.new
   end
 
+  def show
+    @submission_stats = Submission
+      .select(:uuid, :student_id, :quiz_id, 'options.correct AS options_correct', 'COUNT(answers.id) AS answers_count')
+      .group(:id, 'options.correct')
+      .joins(:quiz)
+      .where(quizzes: { cohort_id: @cohort.id })
+      .joins('LEFT JOIN answers ON answers.submission_id = submissions.id')
+      .joins('LEFT JOIN options ON answers.option_id = options.id')
+      .order('options.correct')
+  end
+
   def create
     @cohort = Cohort.new(cohort_params)
     if @cohort.save
