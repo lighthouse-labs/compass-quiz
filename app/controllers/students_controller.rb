e9d5105da9849b3025d838cd3cad8a378165145f
David VanDusen <github@davidvandusen.com>
2016-01-17 10:57:32 -0800

    Add submission stats to studen page.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,14 +3,14 @@ class StudentsController < ApplicationController
   before_action :set_student, only: [:show, :destroy]
 
   def index
-    @students = Student.select('students.*', 'cohorts.*', 'COUNT(submissions.id) AS submissions_count')
-      .joins(:cohort)
+    @students = Student.select('students.*', 'COUNT(submissions.id) AS submissions_count')
       .joins('LEFT JOIN submissions ON submissions.student_id = students.id')
-      .group('students.id', 'cohorts.id')
+      .group('students.id')
       .order(:github_username)
   end
 
   def show
+    @submission_stats = @student.submissions.stats
   end
 
   def destroy
